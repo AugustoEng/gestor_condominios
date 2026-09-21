36660110499fa091ba0d8c3bddc6adffcb639123
Augusto <guilhermesantos172007@gmail.com>
2026-09-21 20:09:20 -0300

    Questão 2

--- src/main/java/gestao_condominio/App.java
+++ src/main/java/gestao_condominio/App.java
@@ -1,19 +1,41 @@
 package gestao_condominio;
+import java.util.Scanner;
 
 public class App {
 
-    static String nome = "Luz Azul";
-    static int blocos = 4;
-    static int unidades = 22;
-    static double taxa = 200.50;
-    static boolean portaria = true;
+    static String nome;
+    static int blocos;
+    static int unidades;
+    static double taxa;
+    static boolean portaria;
 
     public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Insira o Nome: ");
+        nome = scanner.nextLine();
+        System.out.print("Insira os Blocos: ");
+        blocos = scanner.nextInt();
+        System.out.print("Insira as Unidades: ");
+        unidades = scanner.nextInt();
+        System.out.print("Insira a Taxa: ");
+        taxa = scanner.nextDouble();
+        System.out.print("Portaria 24H?\n[1] Sim\n[2] Não\n");
+        if  (scanner.nextInt() == 1)    {
+            portaria = true;
+        } else  {
+            portaria = false;
+        }
+
+
+
         System.out.println("Nome: " + nome);
         System.out.println("Blocos: " + blocos);
         System.out.println("Unidades: " + unidades);
         System.out.println("Taxa padrão: " + taxa);
         System.out.println("Portaria 24H: " + portaria);
+
+        scanner.close();
     }
 
 }

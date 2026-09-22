ba9af22ca17a46879433eed4a59855caa007c0ab
Augusto <guilhermesantos172007@gmail.com>
2026-09-22 19:42:44 -0300

    Questão 10

--- src/main/java/gestao_condominio/Unidade.java
+++ src/main/java/gestao_condominio/Unidade.java
@@ -1,14 +1,30 @@
 package gestao_condominio;
 
 public class Unidade {
-    int numero;
-    double taxaBase;
-    boolean ocupado;
+    private int numero;
+    private double taxaBase;
+    private boolean ocupado;
 
     public Unidade(int numero, double taxaBase, boolean ocupado)    {
         this.numero = numero;
-        this.taxaBase = taxaBase;
+        if  (taxaBase < 0)  {
+            this.taxaBase = 0;
+        } else {
+            this.taxaBase = taxaBase;
+        }
         this.ocupado = ocupado;
+    }
+
+    public int getNumero()  {
+        return numero;
+    }
+
+    public double getTaxaBase() {
+        return taxaBase;
+    }
+
+    public boolean getOcupado() {
+        return ocupado;
     }
 
     public void CalcularTaxa() {

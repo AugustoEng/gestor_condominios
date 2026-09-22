ed8ac336038c02a5a50a7d3ed9690a35968d98cc
Augusto <guilhermesantos172007@gmail.com>
2026-09-21 21:07:01 -0300

    Questão 7

--- src/main/java/gestao_condominio/App.java
+++ src/main/java/gestao_condominio/App.java
@@ -1,75 +1,10 @@
 package gestao_condominio;
-import java.util.Scanner;
 
 public class App {
-
-    static String nome;
-    static int blocos;
-    static int unidades;
-    static double taxa;
-    static boolean portaria;
-
-    static public void Simular(int unidades, double taxa) {
-        double total = 0;
-        int num = 1;
-        for (int i = 0; i < unidades; i++) {
-            System.out.println("Unidade " + num + " - Taxa: " + taxa);
-            total += taxa;
-            num ++;
-        }
-        System.out.printf("Total: %.2f%n", total);
-    }
-
     public static void main(String[] args)  {
-        Scanner scanner = new Scanner(System.in);
-        int decisao = 14;
-
-        while(decisao != 0) {
-            System.out.print("[1] Dados do Condomínio\n[2] Simular Arrecadação\n[0] Sair\n");
-            decisao = scanner.nextInt();
-            scanner.nextLine();
-            if (decisao == 1){
-                System.out.print("Insira o Nome: ");
-                nome = scanner.nextLine();
-                System.out.print("Insira os Blocos: ");
-                blocos = scanner.nextInt();
-                System.out.print("Insira as Unidades: ");
-                unidades = scanner.nextInt();
-                System.out.print("Insira a Taxa: ");
-                taxa = scanner.nextDouble();
-                System.out.print("Portaria 24H?\n[1] Sim\n[2] Não\n");
-                if  (scanner.nextInt() == 1)    {
-                    portaria = true;
-                } else  {
-                    portaria = false;
-                }
-
-                System.out.println("Nome: " + nome);
-                System.out.println("Blocos: " + blocos);
-                System.out.println("Unidades: " + unidades);
-                System.out.print("Taxa padrão: " + taxa);
-                if  (taxa <= 400)   {
-                    System.out.print(" - Taxa Econômica\n");
-                } else if (taxa > 400 && taxa <= 700) {
-                    System.out.print(" - Taxa intermediária\n");
-                } else {
-                    System.out.print(" - Taxa Elevada\n");
-                }
-                System.out.println("Portaria 24H: " + portaria + "\n\n\n");
-            } else if (decisao == 2) {
-                Simular(unidades, taxa);
-            } else if ( decisao == 0) {
-                break;
-            } else {
-                System.out.println("\n\n");
-            }
-        }
-
-
-
-
-
-        scanner.close();
+        Morador Jefferson = new Morador("Jefferson Pereira", "000.000.000-00", 12);
+        System.out.println("Nome " + Jefferson.nome);
+        System.out.println("CPF " + Jefferson.cpf);
+        System.out.println("IDnum " + Jefferson.numeroUnidade);
     }
-
 }
---
+++ src/main/java/gestao_condominio/Morador.java
@@ -0,0 +1,13 @@
+package gestao_condominio;
+
+public class Morador {
+    String nome;
+    String cpf;
+    int numeroUnidade;
+
+    public Morador(String nome, String cpf, int numeroUnidade)  {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.numeroUnidade = numeroUnidade;
+    }
+}

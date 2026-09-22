dd5b80baf968249f61ff95098712d87eb53f25ee
Augusto <guilhermesantos172007@gmail.com>
2026-09-22 19:58:05 -0300

    Questão 11

--- src/main/java/gestao_condominio/App.java
+++ src/main/java/gestao_condominio/App.java
@@ -6,20 +6,25 @@ public class App {
         Unidade Unidade102 = new Unidade(102,20, true);
         Unidade Unidade201 = new Unidade(201, 200, false);
 
-        System.out.print("Unidade 101\n");
-        Unidade101.CalcularTaxa();
-        Unidade101.ExibirSituacao();
-        System.out.print("\n\n");
+//        System.out.print("Unidade 101\n");
+//        Unidade101.CalcularTaxa();
+//        Unidade101.ExibirSituacao();
+//        System.out.print("\n\n");
+//
+//        System.out.print("Unidade 102\n");
+//        Unidade102.CalcularTaxa();
+//        Unidade102.ExibirSituacao();
+//        System.out.print("\n\n");
+//
+//        System.out.print("Unidade 201\n");
+//        Unidade201.CalcularTaxa();
+//        Unidade201.ExibirSituacao();
+//        System.out.print("\n\n");
 
-        System.out.print("Unidade 102\n");
-        Unidade102.CalcularTaxa();
-        Unidade102.ExibirSituacao();
-        System.out.print("\n\n");
+    Condominio VentoAzul = new Condominio("Vento Azul", 3000);
 
-        System.out.print("Unidade 201\n");
-        Unidade201.CalcularTaxa();
-        Unidade201.ExibirSituacao();
-        System.out.print("\n\n");
+    VentoAzul.PagarDespesa(20000);
+    VentoAzul.ReceberPagamento(300);
     }
 }
 
---
+++ src/main/java/gestao_condominio/Condominio.java
@@ -0,0 +1,30 @@
+package gestao_condominio;
+
+public class Condominio {
+    private String nome;
+    private double saldoCaixa;
+
+    public Condominio(String nome, double saldoCaixa)   {
+        this.nome = nome;
+        if  (saldoCaixa < 0)    {
+            this.saldoCaixa = 0;
+        } else  {
+            this.saldoCaixa = this.saldoCaixa;
+        }
+    }
+
+    public void ReceberPagamento(double valor)  {
+        if(valor <= 0)   {
+            System.out.print("\nPagamentos devem ser maiores que Zero\n");
+        } else {
+            System.out.print("\nPagamento Bem sucesido\n");
+        }
+    }
+
+    public void PagarDespesa(double valor)  {
+        if(valor > this.saldoCaixa) {
+            System.out.print("\nSaldo Insuficiente\n");
+        }
+
+    }
+}
